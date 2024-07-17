6174781fc9b25d177409ee8652af7f4963e07036
Skinzin <SkinzinSTG_@hotmail.com>
2024-07-17 19:52:54 -0300

    chore: Add midnight-blue color to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,21 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'midnight-blue': {
+          '50': '#e9fbff',
+          '100': '#cef5ff',
+          '200': '#a7f0ff',
+          '300': '#6bebff',
+          '400': '#26daff',
+          '500': '#00b6ff',
+          '600': '#008cff',
+          '700': '#0071ff',
+          '800': '#0061e6',
+          '900': '#0056b3',
+          '950': '#003870',
+      },
+      }
     },
   },
   plugins: [],
